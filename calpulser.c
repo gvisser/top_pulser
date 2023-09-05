2bc498f1be30d70bcbaad430a41a17c5cab347c7
Gerard Visser <gvisser@indiana.edu>
2023-09-05 10:12:46 -0400

    Generalied/expanded the clock setup handling, variety of tic sizes no supported. (Not all work yet due to FPGA MMCM issues I think.)

--- calpulser.c
+++ calpulser.c
@@ -27,22 +27,42 @@ int main(int argc, char *argv[]) {
   uint8_t tx_buf[32];
   uint8_t rx_buf[32];
   int k,ret;
-  int init_delay=0,pp_delay=20,p_width=5,npulses=2,atten=32;
+  double init_delay_ns=0.0,pp_delay_ns=20.0,p_width_ns=2.0;
+  int init_delay,pp_delay,p_width,npulses=2,atten=32;
+  double tici=0.6,tic,fin=0.040,fout,fvco; // later fin will be set according to CLKSEL control and user input (about ext clock)
+  int pll_R,pll_N,pll_BD,pll_MDA,pll_MDB;
+  double pll_P;
+  const uint16_t MD2M[16] =  // table 15 "MDx[3:0] Programming" in LTC6851 datasheet
+    {0b0000000000000001,
+     0b0000000000000010,
+     0b0000000000000100,
+     0b0000000000001000,
+     0b0000000000001100,
+     0b0000000000010000,
+     0b0000000000011000,
+     0b0000000000100000,
+     0b0000000000110000,
+     0b0000000001000000,
+     0b0000000001100000,
+     0b0000000010000000,
+     0b0000000011000000,
+     0b0000000100000000,
+     0b0000000110000000,
+     0b0000001000000000};
 
-  double tic=0.6;
   while((c = getopt(argc, argv, "t:i:s:w:n:a:")) != -1) {
     switch(c) {
     case 't' :  // time tic in ns (1ns or 600ps, maybe 750, 800 added later))
-      printf("-t isn't supported yet\n");
+      tici=atof(optarg);
       break;
     case 'i' :  // initial delay in ns
-      init_delay = (int) (atof(optarg)/tic + 0.5);
+      init_delay_ns=atof(optarg);
       break;
     case 's' :  // pulse-pulse delay in ns;  REQUIREMENT: pp_delay>=p_width+8  !!!
-      pp_delay = (int) (atof(optarg)/tic + 0.5);
+      pp_delay_ns=atof(optarg);
       break;
     case 'w' :  // pulse width in ns
-      p_width = (int) (atof(optarg)/tic + 0.5);
+      p_width_ns=atof(optarg);
       break;
     case 'n' :  // number of pulses
       npulses = atoi(optarg);
@@ -60,10 +80,70 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  // Clock setups -- add more here -- nothing below '//////' line should be specific to any setup!
+  // For the moment not using delays (but maybe will need to for some clock setups).
+  // For the moment, using local oscillator only, need to work in here the FTSW clock stuff.
+  // Keep fVCO in range 4 - 5 GHz, and take care w/ B value, see datasheet.
+  // Avoid P=x.5 and M=1, due to subharmonic (see datasheet).
+  // Minimum P that fits each case is best, to allow for finest/any control of delays.
+  printf("tici=%lf, fabs(tici-0.5)=%lf\n",tici,fabs(tici-0.5));
+  if (fabs(tici-0.5)<1e-6) {   // need to test    TBD whether the board can run reliably for 0.5 ns tics
+    printf("A\n");
+    pll_R=10;   // range 1 to 63
+    pll_N=500;  // range 2 to 511 (with RA0=1)
+    pll_P=2;    // allowed values 2, 2.5, 3, 3.5, 4
+    pll_BD=1;   // see table 11 "BD[3:0] Programming, RAO = 1" in LTC6951 datasheet
+    pll_MDA=0;  // for outputs 0-3, see table 15 "MDx[3:0] Programming"
+    pll_MDB=3;  // for output 4 (LVDS to FPGA, not really used at the moment)
+  }
+  else if (fabs(tici-0.6)<1e-6) {   // this is probably the best default/suggested tic
+    printf("B\n");
+    pll_R=12;   // range 1 to 63
+    pll_N=500;  // range 2 to 511 (with RA0=1)
+    pll_P=3;    // allowed values 2, 2.5, 3, 3.5, 4
+    pll_BD=1;   // see table 11 "BD[3:0] Programming, RAO = 1" in LTC6951 datasheet
+    pll_MDA=0;  // for outputs 0-3, see table 15 "MDx[3:0] Programming"
+    pll_MDB=3;  // for output 4 (LVDS to FPGA, not really used at the moment)
+  }
+  else if (fabs(tici-1.0)<1e-6) {   // need to test
+    printf("C\n");
+    pll_R=12;   // range 1 to 63
+    pll_N=300;  // range 2 to 511 (with RA0=1)
+    pll_P=2;    // allowed values 2, 2.5, 3, 3.5, 4
+    pll_BD=1;   // see table 11 "BD[3:0] Programming, RAO = 1" in LTC6951 datasheet
+    pll_MDA=1;  // for outputs 0-3, see table 15 "MDx[3:0] Programming"
+    pll_MDB=5;  // for output 4 (LVDS to FPGA, not really used at the moment)
+  }
+  else if (fabs(tici-6.0)<1e-6) {  // need to test   intended for some bench tests only
+    printf("D\n");
+    pll_R=12;   // range 1 to 63
+    pll_N=50;   // range 2 to 511 (with RA0=1)
+    pll_P=3;    // allowed values 2, 2.5, 3, 3.5, 4
+    // following BD setting follows the datasheet guidance but I don't understand it really... p14 oversimplified?
+    pll_BD=6;   // see table 11 "BD[3:0] Programming, RAO = 1" in LTC6951 datasheet
+    pll_MDA=3;  // for outputs 0-3, see table 15 "MDx[3:0] Programming"
+    pll_MDB=9;  // for output 4 (LVDS to FPGA, not really used at the moment)
+  }
+  else {
+    printf("requested time tic value not supported - exiting...\n");
+    return -1;
+  }
+
+  fvco=fin/pll_R*pll_N*pll_P*MD2M[pll_MDA];
+  fout=fin/pll_R*pll_N;
+  tic=1.0/fout;
+
+  init_delay = (int) (init_delay_ns/tic + 0.5);
+  pp_delay = (int) (pp_delay_ns/tic + 0.5);
+  p_width = (int) (p_width_ns/tic + 0.5);
+
+
   printf("TOP pulser setup:\nNote: your selections were rounded to nearest actual values\n");
-  printf("initial delay %d tics (%.2lf ns)\n",init_delay,init_delay*tic);
+  printf("tic size %7.5lf ns\n",tic);
+  printf("  LTC6951 will be set for fout=%7.5lf GHz, fvco=%7.5lf GHz\n",fout,fvco);
+  printf("initial delay %d tics (%.3lf ns)\n",init_delay,init_delay*tic);
   if (npulses==1) {
-    printf("1 pulse, width %d tics (%.2lf ns)\n",p_width,p_width*tic);
+    printf("1 pulse, width %d tics (%.3lf ns)\n",p_width,p_width*tic);
   }
   else {
     printf("%d pulses, ",npulses);
@@ -71,7 +151,7 @@ int main(int argc, char *argv[]) {
       pp_delay = p_width+8;
       printf("\nWARNING: enforced minimum pulse-pulse delay\n");
     }
-    printf("width %d tics (%.2lf ns), pulse-pulse delay %d tics (%.2lf ns)\n",
+    printf("width %d tics (%.3lf ns), pulse-pulse delay %d tics (%.3lf ns)\n",
 	   p_width,p_width*tic,pp_delay,pp_delay*tic);
   }
   printf("amplitude %.3lf V (atten %d dB)\n",1.6*pow(10.0,-atten/20.0),atten);
@@ -84,6 +164,7 @@ int main(int argc, char *argv[]) {
   spit[0].rx_buf = (unsigned long) rx_buf;
   //spit.bits_per_word = 0;
   spit[0].speed_hz = 200000; // MHz, and seems to have good resolution at least for around few MHz
+  // HUH? did I really mean 200 MHZz??? Seems unlikely, should this have been 20?????
   //spit.delay_usecs = 0;
   //    BELOW spit.len = 2;
   spit[1].tx_buf = /*(unsigned long)*/(uint32_t) tx_buf;  // we just use same buffer, for convenience
@@ -153,36 +234,31 @@ int main(int argc, char *argv[]) {
   printf("\n");
   
   // initialize the PLL (LTC6951)
-  // later we'll have a short selection of clock setups to choose between, just change the data here
-  int pll_R=12;    // range 1 to 63
-  int pll_N=500;//300;   // range 2 to 511 (with RA0=1)
-  double pll_P=3;//4;  // allowed values 2, 2.5, 3, 3.5, 4
-  // keep fVCO in range 4 - 5 GHz, and take care w/ B value, see datasheet
-  // R=12, N=450, P=3 works for 1.5 GHz
-  // R=12, N=500, P=3 for 1.666667 GHz (600 ps)
-  // R=12, N=510, P=2.5 works for 1.7 GHz but beware w/ P=2.5 and Mx=1 the duty cycle is not 50%
-  
+  // note the loop filter is designed for fpfd=3.33 MHz, may be best to stay close to that?
+  // it would probably be best to check bounds on pll_R, pll_N, pll_P, pll_MDA, pll_MDB here... Add this someday
+  // while at that, can check VCO frequency restriction automatically here too...
+
   tx_buf[0] = 0x01*2+0;  // start write at register 0x01
   tx_buf[1] = 0xba;   // reg 1: STAT = not (ALCHI or ALCLO or (not LOCK) or (not REFOK)) = "PLLOK"
   tx_buf[2] = 0x04;   // reg 2: SYNC asserted; nothing powered down; for cal we use autocal so don't set it here
   tx_buf[3] = 0x7e;   // reg 3: ALC: during cal only, monitor level always, autocal enabled, RA0=1, low level ref input
-  tx_buf[4] = 0x17;   // reg 4: B=12 (for fPFD=3.333 MHz), LKWIN 10.7ns, LKCNT 2048
-  // it would seem I didn't really implement pll_R here -- FIX THIS! !! !!! !!!! !!!!!
-  tx_buf[5] = 0x30 | ((pll_N&0x300)>>8); //0x31;    // |
-  tx_buf[6] = (pll_N&0xff);    // | R=12, N=300
+  tx_buf[4] = (pll_BD<<4) | 0x07;                  // reg 4: BD, LKWIN 10.7ns, LKCNT 2048
+  tx_buf[5] = (pll_R<<2) | ((pll_N&0x300)>>8);     // reg 5: R and top 2 bits of N
+  tx_buf[6] = (pll_N&0xff);                        // reg 6: bottom byte of N
   tx_buf[7] = 0x05;   // reg 7: CP: no overrides, not WIDE, 5.6 mA
-  tx_buf[8] = (((int)((pll_P-1.999)*2))&0x07)<<5;    // no mute
-  tx_buf[9] = 0x80;   // reg 8: out0: sync enable, no mute, M=1
+  tx_buf[8] = (((int)((pll_P-1.999)*2))&0x07)<<5;  // reg 8: P, no mute
+  tx_buf[9] = 0x80 | pll_MDA;   // reg 8: out0: sync enable, no mute, MDA
   tx_buf[10] = 0x00;  // reg 9: SN=0 SR=0
-  tx_buf[11] = 0x80;  // reg 10: out1: sync enable, no mute, M=1
+  tx_buf[11] = 0x80;  // reg 10: out1: sync enable, no mute, MDA
   tx_buf[12] = 0x00;  // reg 11: out1: delay 0
-  tx_buf[13] = 0x80;  // reg 13: out2: sync enable, no mute, M=1
+  tx_buf[13] = 0x80;  // reg 13: out2: sync enable, no mute, MDA
   tx_buf[14] = 0x00;  // reg 14: out2: delay 0
-  tx_buf[15] = 0x80;  // reg 15: out3: sync enable, no mute, M=1
+  tx_buf[15] = 0x80;  // reg 15: out3: sync enable, no mute, MDA
   // best polarity for various clocks has to be investigated still   !!! CAUTION !!!
   tx_buf[16] = 0x40;  // reg 16: out3: delay 0, let's invert (better FF clock timing?)
-  tx_buf[17] = 0x83;  // reg 17: out4: sync enable, no mute, M=8
-  tx_buf[18] = 0x06;  // reg 18: out4: delay 6 (this gets it phase aligned with out0-3)
+  tx_buf[17] = 0x83;  // reg 17: out4: sync enable, no mute, MDB (!!)
+  tx_buf[18] = 0x06;  // reg 18: out4: delay 6 (this gets it phase aligned with out0-3)  THIS IS NOT RIGHT DELAY IF MDB/=3
+  // I NEED TO REALLY HANDLE THE DELAYS IN VARIOUS CLOCK SETUPS, add this, probably needed for phase details on othe clocks anyway
   spit[1].len = 19;
   ret = ioctl(spifd[1], SPI_IOC_MESSAGE(1), &spit[1]);
   if(ret<0) {
@@ -207,7 +283,7 @@ int main(int argc, char *argv[]) {
   else
     printf("WARNING: LTC6951 status reads no good! 0x%02x\n",rx_buf[1]);
 
-  // do MC100EP446 SYNC stuff now (not implemented yet)
+  // release MC100EP446 SYNC
   printf("releasing MC100EP446 SYNC...\n");
   tx_buf[0] = 0x80; // keep device 1 to the PLL (using device 0), and release MC100EP446 SYNC
   tx_buf[1] = 0x00;
@@ -303,12 +379,6 @@ int main(int argc, char *argv[]) {
   printf("\n");
   usleep(50000);
   // clocks are all stable (one hopes) and we are addressed to VGA A, proceed with that
-
-  // NOTE: FOR NOW, the channels will not be properly synchronized despite we did this probably correctly (TBD).
-  // I snafu'd the board and used OUT0 for channel B serializer, but OUT0 does not mute for the LTC6951 SYNC, and so
-  // channel B serializer has no chance to get in sync with channel A one. I could set RAO=0 for the LTC6951 but that will
-  // cause problems to use FTSW trigger/clock input (I think). Or I could (maybe) patch the board to swap the clock lines
-  // of OUT1 and OUT0, which will put OUT0 to one of the pulser FF's (which don't care about this snafu). Will try that.
 
   // set gain
   tx_buf[0] = 0x02;
